957a501f5a23a7932c89ff5b16ec6c9fddd3e184
seonbo.shim <seonbo.shim@bespinglobal.com>
2024-01-27 04:54:00 +0900

    DEVT-5335 tfmodule-aws-s3 - s3_bucket_server_side_encryption 구성 보완

--- s3-encrypt.tf
+++ s3-encrypt.tf
@@ -1,14 +1,3 @@
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
 locals {
   kms_master_key_id  = var.kms_master_key_id != null ? var.kms_master_key_id : var.sse_algorithm == "aws:kms" ? "aws/s3" : null
   bucket_key_enabled = var.sse_algorithm == "aws:kms" ? var.bucket_key_enabled : null
